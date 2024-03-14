7cb45868d1461d7a7cf69c5afe5dc24c507d4ae1
이준원 <junwlee@ijun-won-ui-MacBookPro.local>
2024-03-14 15:55:01 +0900

    Simplify the code

--- Chapter1/sumDigitsInputProgram.py
+++ Chapter1/sumDigitsInputProgram.py
@@ -1,12 +1,3 @@
 string_int = input("Write Integer: ")
-string_length = len(string_int)
-integer_int = int(string_int)
-lst = [i for i in range(string_length)]
-sortedListDesc = sorted(lst, reverse=True)
-
-digitSum = 0
-for j in sortedListDesc:
-  digitSum += (integer_int // (10 ** j))
-  integer_int -= (integer_int // (10 ** j)) * (10 ** j)
-
+digitSum = sum(int(digit) for digit in string_int)
 print(digitSum)
