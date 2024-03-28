e5f70ffee03067ac63e890ab991c7b2d02267f7b
이준원 <junwlee@ijun-won-ui-MacBookPro.local>
2024-03-28 11:29:17 +0900

    Add restrict(Solve the infinite loop).

--- Chapter3/CoinFlip.py
+++ Chapter3/CoinFlip.py
@@ -1,15 +1,28 @@
 import random
 
+# Randomly choose 0 or 1, representing heads or tails
 coin = random.randrange(2)
-pick = int(input("Write a number between 0 or 1: "))
+
+# Initialize the number of attempts
 chance = 0
+
 while True:
-    if pick == coin:
-        print("You won!")
-        chance += 1
-        break
-    else:
-        print("You lost!")
+    # Prompt the user for their guess
+    pick = int(input("Guess the coin side (0 for heads, 1 for tails): "))
+
+    # Check if the input is either 0 or 1
+    if pick == 0 or pick == 1:
+        # Increment the attempt counter
         chance += 1
 
-print("You won", chance, "")
+        # Check if the guess matches the coin toss
+        if coin == pick:
+            print("Congratulations! You guessed it right.")
+            break
+        else:
+            print("Try again.")
+    else:
+        print("Please enter 0 or 1 for your guess.")
+
+# Print the total number of attempts
+print(f'Your tries: {chance}')
